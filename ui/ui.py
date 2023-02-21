1cb9fee6e4978405a61536ff0fb81c3a320db4bd
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-21 08:33:53 +0100

    Add input widgets

--- ui/ui.py
+++ ui/ui.py
@@ -2,11 +2,13 @@ import customtkinter
 
 class Ui:
     def __init__(self, app):
-        self.create_widgets(app)
+        mainColor = "#F8F4EA"
+        secondColor = "#579BB1"
+        self.create_widgets(app, mainColor, secondColor)
 
-    def create_widgets(self, app):
+    def create_widgets(self, app, color, secondColor):
 #--------------create grid frames-------------------------------------
-        inputFrame = customtkinter.CTkFrame(app, width=640, height=90, fg_color="red")
+        inputFrame = customtkinter.CTkFrame(app, width=640, height=90, fg_color=color)
         inputFrame.grid(row = 0, column = 0, columnspan = 3, sticky = "NSWE")
 
         outputFrame = customtkinter.CTkFrame(app, width=640, height=90, fg_color="yellow")
@@ -25,8 +27,18 @@ class Ui:
         emptyFrame.grid(row = 7, column = 4, columnspan = 2, sticky = "NSWE")
 
 #---------------configure grid----------------------------------------------
-
         app.columnconfigure((0,1,2,3,4,5), weight=1)
         app.rowconfigure((0, 7), weight = 2)
         app.rowconfigure((1,2,3,4,5,6), weight = 3)
+
+        inputFrame.columnconfigure((0, 1, 2, 3), weight=1)
+        inputFrame.rowconfigure(0, weight=1)
+
+#---------------add widgets----------------------------------------------
+        inputInfoLabel = customtkinter.CTkLabel(inputFrame, text = "Ścieżka do pliku:", font = ("Arial", 20), anchor="nw")
+        inputInfoLabel.grid(row = 0, column = 0, columnspan = 3, sticky = "NSWE", padx = 15, pady = 15)
+
+        inputButton = customtkinter.CTkButton(inputFrame, text = "Wybierz", font = ("Arial", 19), fg_color=secondColor)
+        inputButton.grid(row = 0, column = 3, sticky = "NSWE", padx = 15, pady = 15)
+
 
