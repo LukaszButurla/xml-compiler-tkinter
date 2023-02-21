5669daaa52144792db53a96bef33f8b8074801c3
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-21 08:55:56 +0100

    Add convert button

--- ui/ui.py
+++ ui/ui.py
@@ -20,10 +20,10 @@ class Ui:
         infoFrame = customtkinter.CTkFrame(app, width=120, height=90, fg_color=color)
         infoFrame.grid(row = 7, column = 0, columnspan = 2, sticky = "NSWE")
 
-        buttonFrame = customtkinter.CTkFrame(app, width=240, height=90, fg_color="brown")
+        buttonFrame = customtkinter.CTkFrame(app, width=240, height=90, fg_color=color)
         buttonFrame.grid(row = 7, column = 2, columnspan = 2, sticky = "NSWE")
 
-        emptyFrame = customtkinter.CTkFrame(app, width=120, height=90, fg_color="orange")
+        emptyFrame = customtkinter.CTkFrame(app, width=120, height=90, fg_color=color)
         emptyFrame.grid(row = 7, column = 4, columnspan = 2, sticky = "NSWE")
 
 #---------------configure grid----------------------------------------------
@@ -40,6 +40,9 @@ class Ui:
         infoFrame.columnconfigure((0, 1, 2, 3), weight=1)
         infoFrame.rowconfigure(0, weight=1)
 
+        buttonFrame.columnconfigure(0, weight=1)
+        buttonFrame.rowconfigure(0, weight=1)
+
 #---------------add widgets----------------------------------------------
         inputInfoLabel = customtkinter.CTkLabel(inputFrame, text = "Ścieżka do pliku:", font = ("Arial", 20), anchor="nw")
         inputInfoLabel.grid(row = 0, column = 0, columnspan = 3, sticky = "NSWE", padx = 15, pady = 15)
@@ -55,5 +58,8 @@ class Ui:
 
         infoButton = customtkinter.CTkButton(infoFrame, text = "O programie", font = ("Arial", 19), fg_color=secondColor)
         infoButton.grid(row = 0, column = 0, padx = 15, pady = 15, sticky = "NSWE")
+
+        convertButton = customtkinter.CTkButton(buttonFrame, text = "Potwierdź", font = ("Arial", 19), fg_color=secondColor)
+        convertButton.grid(row = 0, column = 0, sticky= "NSWE", padx = 30, pady = 15)
 
 
