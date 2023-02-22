7189c81d76f2873dc338b627c6987abd575e5f8f
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-22 08:27:33 +0100

    Create functionality to read data from version 2

--- logic/getData.py
+++ logic/getData.py
@@ -43,6 +43,31 @@ class GetData:
 
                 case 2:
                     print(2)
+                    amountOfSubjects = lines[lines.find("<LiczbaPozycji>")+15:lines.find("</LiczbaPozycji>")]
+                    nip = "6970011183"
+
+                    for subject in range(int(amountOfSubjects)):
+
+                        subjectStart = lines.find("<Pozycja>")
+                        subjectEnd = lines.find("</Pozycja>")
+                        subject = lines[subjectStart:subjectEnd]
+
+                        description = subject[subject.find("<Opis>")+6:subject.find("</Opis>")]
+                        index = subject[subject.find("<Indeks>")+8:subject.find("</Indeks>")]
+                        unit = subject[subject.find("<Jednostka>")+11:subject.find("</Jednostka>")]
+                        price = subject[subject.find("<CenaPLN>")+9:subject.find("</CenaPLN>")]
+                        priceNetto = subject[subject.find("<WartoscNetto>")+14:subject.find("</WartoscNetto>")]
+                        vat = subject[subject.find("<Procent>")+9:subject.find("</Procent>")][:2]
+                        amount = subject[subject.find("<Ilosc>")+7:subject.find("</Ilosc>")]
+                        group = "Grupa Główna"
+
+                        vatFloat = "0.{}".format(vat)
+                        priceVat = "{:.2f}".format(float(priceNetto.replace(",", ".")) * float(vatFloat))
+
+                        lines = lines[subjectEnd+10:]
+                        
+                        print(index, description, unit, price, priceNetto, priceVat, vat, amount)
+
 
                 case 3:
                     print(3)
@@ -54,6 +79,7 @@ class GetData:
             if "<NIP>" in lines:
                 seller = lines[lines.find('<StronaUmowy kto="Sprzedajacy">'):lines.find("</StronaUmowy>")]
                 nip = seller[seller.find("<NIP>")+5:seller.find("</NIP>")].replace("-", "")
+                print(nip)
 
                 if nip == "7741008197":
                     version = 1
