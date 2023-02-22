332ab6b06aa648c4dfaa824bf5a37a76e8cc77d6
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-22 13:25:40 +0100

    Format table text to 2 decimal numbers

--- logic/getData.py
+++ logic/getData.py
@@ -33,11 +33,11 @@ class GetData:
                         description = "{} {}".format(description1, descripton2)
                         index = subject[subject.find("<Indeks>")+8:subject.find("</Indeks>")]
                         unit = subject[subject.find("<Jednostka>")+11:subject.find("</Jednostka>")]
-                        price = subject[subject.find("<Cena>")+6:subject.find("</Cena>")]
-                        priceNetto = subject[subject.find("<WartoscNetto>")+14:subject.find("</WartoscNetto>")]
-                        priceVat = subject[subject.find("<WartoscVAT>")+12:subject.find("</WartoscVAT>")]
+                        price = "{:.2f}".format(float(subject[subject.find("<Cena>")+6:subject.find("</Cena>")].replace(",", ".")))
+                        priceNetto = "{:.2f}".format(float(subject[subject.find("<WartoscNetto>")+14:subject.find("</WartoscNetto>")]))
+                        priceVat = "{:.2f}".format(float(subject[subject.find("<WartoscVAT>")+12:subject.find("</WartoscVAT>")]))
                         vat = subject[subject.find("<Procent>")+9:subject.find("</Procent>")][1:]
-                        amount = subject[subject.find("<Ilosc>")+7:subject.find("</Ilosc>")]
+                        amount = "{:.0f}".format(float(subject[subject.find("<Ilosc>")+7:subject.find("</Ilosc>")]))
                         group = "Grupa Główna"
                         priceVatAll += float(priceVat)
                         priceNettoAll += float(priceNetto)
@@ -61,10 +61,10 @@ class GetData:
                         description = subject[subject.find("<Opis>")+6:subject.find("</Opis>")]
                         index = subject[subject.find("<Indeks>")+8:subject.find("</Indeks>")]
                         unit = subject[subject.find("<Jednostka>")+11:subject.find("</Jednostka>")]
-                        price = subject[subject.find("<CenaPLN>")+9:subject.find("</CenaPLN>")]
-                        priceNetto = subject[subject.find("<WartoscNetto>")+14:subject.find("</WartoscNetto>")].replace(",", ".")
+                        price = "{:.2f}".format(float(subject[subject.find("<CenaPLN>")+9:subject.find("</CenaPLN>")].replace(",", ".")))
+                        priceNetto = "{:.2f}".format(float(subject[subject.find("<WartoscNetto>")+14:subject.find("</WartoscNetto>")].replace(",", ".")))
                         vat = subject[subject.find("<Procent>")+9:subject.find("</Procent>")][:2]
-                        amount = subject[subject.find("<Ilosc>")+7:subject.find("</Ilosc>")]
+                        amount = "{:.0f}".format(float(subject[subject.find("<Ilosc>")+7:subject.find("</Ilosc>")]))
                         group = "Grupa Główna"
 
                         vatFloat = "0.{}".format(vat)
@@ -93,11 +93,11 @@ class GetData:
                         description = subject[subject.find("<opisProduktu>")+14:subject.find("</opisProduktu>")]
                         index = subject[subject.find("<indeks>")+8:subject.find("</indeks>")]
                         unit = subject[subject.find("<jednostka>")+11:subject.find("</jednostka>")]                          
-                        price = subject[subject.find("<cenaJednPrzedRabatem>")+22:subject.find("</cenaJednPrzedRabatem>")]
-                        priceNetto = subject[subject.find("<kwotaNetto>")+12:subject.find("</kwotaNetto>")].replace(",", ".")
-                        priceVat = subject[subject.find("<kwotaVAT>")+10:subject.find("</kwotaVAT>")].replace(",", ".")
+                        price = "{:.2f}".format(float(subject[subject.find("<cenaJednPrzedRabatem>")+22:subject.find("</cenaJednPrzedRabatem>")].replace(",", ".")))
+                        priceNetto = "{:.2f}".format(float(subject[subject.find("<kwotaNetto>")+12:subject.find("</kwotaNetto>")].replace(",", ".")))
+                        priceVat = "{:.2f}".format(float(subject[subject.find("<kwotaVAT>")+10:subject.find("</kwotaVAT>")].replace(",", ".")))
                         vat = subject[subject.find("<kodVAT>")+10:subject.find("</kodVAT>")]
-                        amount = subject[subject.find("<ilosc>")+7:subject.find("</ilosc>")]  
+                        amount = "{:.0f}".format(float(subject[subject.find("<ilosc>")+7:subject.find("</ilosc>")].replace(",", ".")))
                         group = "Grupa Główna"
 
                         priceVatAll += float(priceVat)
