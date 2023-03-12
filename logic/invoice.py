e25723db6974af3706124a914b958f584c671f60
DESKTOP-HF35O0S\lukasz <lukasz.buturla.25@gmail.com>
2023-03-12 21:33:23 +0100

    Add next file to create

--- logic/invoice.py
+++ logic/invoice.py
@@ -6,7 +6,7 @@ class Invoice:
         pass
     
     def create_invoice(self, savePath, data, nip):
-        fileName1 = "faktura_{}_,.xls".format(nip)
+        fileName1 = "faktura_{}_,_stary.xls".format(nip)
         savePath1 = os.path.join(savePath, fileName1)
 
         with open(savePath1, "w+", encoding="utf-8", newline="") as fSave:
@@ -16,7 +16,7 @@ class Invoice:
                 values = [d[0], d[2], d[3]]
                 writer.writerow(values)
                 
-        fileName2 = "faktura_{}_;.xls".format(nip)
+        fileName2 = "faktura_{}_;_stary.xls".format(nip)
         savePath2 = os.path.join(savePath, fileName2)
 
         with open(savePath2, "w+", encoding="utf-8", newline="") as fSave:
@@ -25,5 +25,17 @@ class Invoice:
             for d in data:
                 values = [d[0], d[2], d[3]]
                 writer.writerow(values)
+                
+        fileName3 = "faktura_{}_;_nowy.xls".format(nip)
+        savePath3 = os.path.join(savePath, fileName3)
+        
+        with open(savePath3, "w+", encoding="utf-8", newline="") as fSave:
+            writer = csv.writer(fSave, delimiter=";")
+
+            for d in data:
+                values = [d[0], d[2], d[3].replace(",", ".")]
+                writer.writerow(values)
+                
+                
 
                 
